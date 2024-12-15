4088f8ee59c1ac88e6c235f8f9373a7abef11721
louis <1221432@isep.ipp.pt>
2024-12-15 15:53:01 +0000

    updated UserLinkScreen.tsx

--- LibraryApp/app/screens/UserLinkScreen.tsx
+++ LibraryApp/app/screens/UserLinkScreen.tsx
@@ -1,5 +1,6 @@
-import React, { useEffect, useState } from 'react';
+import * as React from 'react';
 import { View, Text, StyleSheet, FlatList, Image, Alert, TouchableOpacity } from 'react-native';
+import {useEffect, useState} from "react";
 
 interface Book {
     title: string;
